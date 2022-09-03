8658402fc80846fae3b1a2029c824fe8faeadf17
Minjae Cho <ussr1285@ajou.ac.kr>
2022-09-03 23:23:21 +0900

    tutorial2
    aws랑 꼬여서 잠시 깃허브로 업로드

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,14 @@
-// Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
-// Step 2: Define your component
 const AboutPage = () => {
     return (
-        <main>
-            <h1>About Me</h1>
-	    <Link to="/">Back to Home</Link>
+        <Layout pageTitle="About Me">
             <p>Hi there! I'm the proud? creator of this site, which I built with Gatsby.</p>
-        </main>
+        </Layout>
     )
 }
 
 export const Head = () => <title>About Me</title>
 
-// Step 3: Export your component
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,16 @@
-// Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Layout pageTitle="Home Page">
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+      </Layout>
     </main>
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>
 
-// Step 3: Export your component
 export default IndexPage
