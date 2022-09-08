a451ffdcabe28ff3894b57664ca2c6636ef09299
Minjae Joe(Cho) <ussr1285@ajou.ac.kr>
2022-09-08 23:43:58 +0900

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -12,6 +12,7 @@ const BlogPage = ({ data }) => {
                         <article key={node.id}>
                             <h2>{node.frontmatter.title}</h2>
                             <p>Posted: {node.frontmatter.date}</p>
+                            <p>{node.excerpt}</p>
                         </article>
                     ))
                 }
